076f14840aa3dac1191385dfe69a236ff2af1fa6
Dingchao Zhang <dingchaoie@gmail.com>
2021-07-23 22:49:34 -0500

    add NFT contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identfier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./Counters.sol";
+
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor (address marketplaceAddress) ERC721("NFT Market","NMART") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender,newItemId);
+        _setTokenURI(newItemId,tokenURI);
+        setApprovalForAll(contractAddress,true);
+        return newItemId;
+    }
+
+
+}
