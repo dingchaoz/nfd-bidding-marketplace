964b4b8c6824be6dd3d96eb50d4ef39f306c39e5
Dingchao Zhang <dingchaoie@gmail.com>
2021-07-24 00:30:09 -0500

    define beforeEach init in test

--- test/sample-test.js
+++ test/sample-test.js
@@ -4,19 +4,31 @@ const { expect,assert} = require("chai");
 const ether = require("@openzeppelin/test-helpers/src/ether");
 const DAY = 86400;
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Marketplace contract", function() {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  let Market;
+  let market;
+  let marketAddress;
+  let NFT;
+  let nft;
+  let creator;
+  let addr1;
+  let addr2;
+  let addr3;
+  let provider;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  this.beforeEach(async function () {
+    Market = await ethers.getContractFactory("NFTMarket")
+    market = await Market.deploy()
+    await market.deployed()
+    marketAddress = market.address
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    NFT = await ethers.getContractFactory("NFT")
+    nft = await NFT.deploy(marketAddress)
+    await nft.deployed();
+    [creator, addr1, addr2, addr3] = await ethers.getSigners()
+    provider = ethers.provider
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
-});
+
+})
