5a414f354c9e5a20851247aaf64e101f831c4074
Dingchao Zhang <dingchaoie@gmail.com>
2021-07-24 00:53:27 -0500

    adding other tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -80,4 +80,90 @@ describe("Marketplace contract", function() {
 
   });
 
+  describe("Withdraw bid", function(){
+
+    it("Should be able to withdraw", async function() {
+    let listingPrice = await market.getListingPrice()
+    listingPrice = ethers.BigNumber.from(listingPrice.toString())
+
+    incrementalBid= ethers.BigNumber.from("1000000000000000000");
+    console.log(ethers.utils.formatUnits(incrementalBid.add(listingPrice),18))
+
+  
+    let nftContractAddress = nft.address
+    let minPrice = ethers.utils.parseUnits('1', 'ether')
+    let auctionPrice = ethers.utils.parseUnits('10', 'ether')
+    let higherAuctionPrice = ethers.utils.parseUnits('20', 'ether')
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await market.createBidItem(nftContractAddress, 1, minPrice, 10000,{value: listingPrice })
+    await market.connect(addr3).bid(1, { value: auctionPrice})
+    await market.connect(addr2).bid(1, { value: higherAuctionPrice})
+    prev_balance = await (provider.getBalance(addr3.address))
+    await market.connect(addr3).withdraw()
+    new_balance = await provider.getBalance(addr3.address)
+    new_balance = ethers.utils.formatEther(new_balance)
+    prev_balance = ethers.utils.formatEther(prev_balance)
+    expect(ethers.utils.parseUnits(new_balance,"ether")).to.be.above(ethers.utils.parseUnits(prev_balance,"ether"))
+    });
+
+  });
+
+  describe("Fetch market items", function(){
+
+    it("Should be able to get all unsold items", async function() {
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    let nftContractAddress = nft.address
+    let minPrice = ethers.utils.parseUnits('1', 'ether')
+    let auctionPrice = ethers.utils.parseUnits('10', 'ether')
+    let higherAuctionPrice = ethers.utils.parseUnits('20', 'ether')
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await market.createBidItem(nftContractAddress, 1, minPrice, 10000,{value: listingPrice })
+    await nft.createToken("https://www.mytokenlocation2.com")
+    await market.createBidItem(nftContractAddress, 2, minPrice, 10000,{value: listingPrice })
+    await market.connect(addr3).bid(1, { value: auctionPrice})
+    await market.connect(addr2).bid(1, { value: higherAuctionPrice})
+
+    items = await market.fetchBidItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.minPrice.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+    
+    });
+
+  });
+
+  describe("Close Auction", function(){
+
+    it("Should transfer ownership to highest bidder", async function() {
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    let nftContractAddress = nft.address
+    let minPrice = ethers.utils.parseUnits('1', 'ether')
+    let auctionPrice = ethers.utils.parseUnits('10', 'ether')
+    let higherAuctionPrice = ethers.utils.parseUnits('20', 'ether')
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await market.createBidItem(nftContractAddress, 1, minPrice, 10000,{value: listingPrice })
+    await market.connect(addr3).bid(1, { value: auctionPrice})
+    await market.connect(addr2).bid(1, { value: higherAuctionPrice})
+    await network.provider.send("evm_increaseTime", [10000])
+    await market.connect(creator).auctionEnd(nftContractAddress,1)
+    const tokenOwner = await nft.ownerOf(1);
+    assert.equal(tokenOwner, addr2.address)
+    });
+
+  });
+
 })
