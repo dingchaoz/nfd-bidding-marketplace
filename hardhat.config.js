e6beec8cac93dd5aa22d9b986a4ec7a3cf3114df
Dingchao Zhang <dingchaoie@gmail.com>
2021-07-23 22:46:52 -0500

    update hardhat config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +19,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks:{
+    hardhat: {
+      chainId : 1337
+    }
+  },
   solidity: "0.8.4",
 };
